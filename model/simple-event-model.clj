2bf594081771a518cd10ba8ba4d097234a3d32a2
Julian Kelsey <julian.b.kelsey@gmail.com>
2013-03-25 16:00:00 +0000

    change of strategy for creating event sequences so that event ids are more explicit allowing messages to be more clearly associated with events for other processes

--- model/simple-event-model.clj
+++ model/simple-event-model.clj
@@ -1,18 +1,20 @@
-(defn event [proc-id [[event-type & msg] event-id]] 
-  {:process proc-id 
-   :event-id (keyword (str "e" event-id)) 
-   :type (condp = event-type
-            :local   :local
-            :send    [:send (first msg) (second msg)]
-            :receive [:receive (first msg) (second msg)])})
+(defn e 
+  ([start] (e start start))
+  ([start finish] (map #(keyword (str "e" %)) (range start (inc finish)))))
+
+(defn msg 
+  ([[e] t c] [[e t c]])
+  ([[e] t c [m]] [[e t c m]]))
 
 (defn events [proc-id es]
-  (let [es (mapcat #(if (number? %) (repeat % '(:local)) (list %)) es)
-        es (map vector es (range))
-        e  (partial event proc-id)]
-    [proc-id (map e es)]))
+  [proc-id (apply concat es)])
 
-(def dist1 
-  (into {}
-    [(events :p0 [1 '(:send :p1 :u1) 2])
-     (events :p1 [4 '(:receive :p0 :u1)])]))  
+(defn receive? [e]
+  (and
+    (coll? e)
+    (not (empty? (filter #(= :rec %) e))) ))
+
+(def dist2 
+  (into {} [
+    (events :p0 [(e 0 1) (msg (e 2) :send :p1) (e 3)])
+    (events :p1 [(e 0) (msg (e 1) :rec :p0 (e 2)) (e 2 4)])]))
