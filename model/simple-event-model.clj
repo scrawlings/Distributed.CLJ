124ece89d43bb39ae781ec6a5bf20791d1641377
Julian Kelsey <julian.b.kelsey@gmail.com>
2013-03-25 15:05:00 +0000

    unique identifiers on interprocess messages

--- model/simple-event-model.clj
+++ model/simple-event-model.clj
@@ -1,10 +1,10 @@
-(defn event [proc-id [[event-type & event-partner] event-id]] 
+(defn event [proc-id [[event-type & msg] event-id]] 
   {:process proc-id 
-   :event-id event-id 
+   :event-id (keyword (str "e" event-id)) 
    :type (condp = event-type
             :local   :local
-            :send    [:send (first event-partner)]
-            :receive [:receive (first event-partner)])})
+            :send    [:send (first msg) (second msg)]
+            :receive [:receive (first msg) (second msg)])})
 
 (defn events [proc-id es]
   (let [es (mapcat #(if (number? %) (repeat % '(:local)) (list %)) es)
@@ -14,5 +14,5 @@
 
 (def dist1 
   (into {}
-    [(events :p0 [1 '(:send :p1) 2])
-     (events :p1 [4 '(:receive :p0)])]))  
+    [(events :p0 [1 '(:send :p1 :u1) 2])
+     (events :p1 [4 '(:receive :p0 :u1)])]))  
